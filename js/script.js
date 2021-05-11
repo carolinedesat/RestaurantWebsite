ede59ede1fe21918f2ce8ea320054c3b5243a41e
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2021-05-11 14:03:23 +0100

    mains, starters, desserts and drinks bill

--- js/script.js
+++ js/script.js
@@ -9,29 +9,29 @@ var number = document.getElementById('number');
 var length = document.getElementById('length');
 
 //shows the message box when the user clicks on the password field
-myInput.onfocus = function() {
+myInput.onfocus = function () {
   document.getElementById('message').style.display = block;
 }
 
 //hides the message box when the user clicks on the password field
-myInput.onblur = function() {
+myInput.onblur = function () {
   document.getElementById('message').style.display = 'none';
 }
 
-myInput.onkeyup = function() {
+myInput.onkeyup = function () {
   //validates lowercase letters
   var lowerCaseLetters = /[a-z]/g;
-  if(myInput.value.match(lowerCaseLetters)) {  
+  if (myInput.value.match(lowerCaseLetters)) {
     letter.classList.remove("invalid");
     letter.classList.add("valid");
   } else {
     letter.classList.remove("valid");
     letter.classList.add("invalid");
   }
-  
+
   //validates uppercase letters
   var upperCaseLetters = /[A-Z]/g;
-  if(myInput.value.match(upperCaseLetters)) {  
+  if (myInput.value.match(upperCaseLetters)) {
     capital.classList.remove("invalid");
     capital.classList.add("valid");
   } else {
@@ -41,16 +41,16 @@ myInput.onkeyup = function() {
 
   //validates numbers
   var numbers = /[0-9]/g;
-  if(myInput.value.match(numbers)) {  
+  if (myInput.value.match(numbers)) {
     number.classList.remove("invalid");
     number.classList.add("valid");
   } else {
     number.classList.remove("valid");
     number.classList.add("invalid");
   }
-  
+
   //validates length
-  if(myInput.value.length >= 8) {
+  if (myInput.value.length >= 8) {
     length.classList.remove("invalid");
     length.classList.add("valid");
   } else {
@@ -63,27 +63,28 @@ myInput.onkeyup = function() {
   RANDOM USER GENERATOR
 */
 
+//everytime the button with ID "getRandom" is clicked the getRandom() function will be called
 document.getElementById('getRandom').addEventListener('click', getRandom);
 
-function getRandom() {
-    fetch('https://randomuser.me/api/?results=1')
+function getRandom() { //creates the function that will be called
+  fetch('https://randomuser.me/api/?results=5') //random user API with 5 results
     .then((res) => res.json())
     .then((data) => {
-        let author = data.results;
-        let output = ''
-        console.log(data);
-        author.forEach(function(user){
-            output += `
+      let author = data.results;
+      let output = ''
+      console.log(data);
+      author.forEach(function (user) {
+        //specifies the output format (medium picture, first and last names and phone number)
+        output += `
             <div>
-                <div><img src="${user.picture.large}"></div>
+                <div><img src="${user.picture.medium}"></div>
                   <p>Name: ${user.name.first} ${user.name.last}</p>
-                  <p>Email: ${user.email}</p>
                   <p>Phone Number: ${user.phone}</p>
             </div>
             `;
-        });
-
-        document.getElementById('output').innerHTML = output;
+      });
+      //shows the output format
+      document.getElementById('output').innerHTML = output;
 
     })
 }
@@ -92,19 +93,39 @@ function getRandom() {
   MENU
 */
 
+//overall bill
 var total_items = 12;
 
-function CalculateItemsValue(){
-    var total = 0;
+function CalculateItemsValue() { //creates the function that will be called
+  var total = 0;
 
-    for (let i = 1; i <= total_items; i++) {
-       itemID = document.getElementById("qnt" + i);
-       total = total + parseFloat(itemID.value) * parseFloat(itemID.getAttribute("data-price"));
-        
-    }
-    document.getElementById('ItemsTotal').innerHTML = "€" + total;
+  for (let i = 1; i <= total_items; i++) {
+    itemID = document.getElementById("qnt" + i);
+    total = total + parseFloat(itemID.value) * parseFloat(itemID.getAttribute("data-price"));
+
+  }
+  document.getElementById('ItemsTotal').innerHTML = "€" + total;
 }
 
 document.querySelectorAll('[id^="qnt"]').forEach(item => {
-    item.addEventListener('keyup', CalculateItemsValue);
+  item.addEventListener('keyup', CalculateItemsValue);
 });
+
+//starters, mains, desserts and drinks bill
+function GetCategoryTotal(categoryDivId) {
+  inputs = document.getElementById(categoryDivId).getElementsByTagName('input');
+  var categoryTotal = 0;
+
+  for (let i = 1; i <= inputs.count; i++) {
+      item = inputs[i]
+      categoryTotal = categoryTotal + parseFloat(item.value) * parseFloat(item.getAttribute("data-price"));
+  }
+  
+  return categoryTotal;
+}
+
+document.getElementById('StartersTotal').innerHTML = "€" + GetCategoryTotal("starters");
+document.getElementById('MainsTotal').innerHTML = "€" + GetCategoryTotal("mains");
+document.getElementById('DessertsTotal').innerHTML = "€" + GetCategoryTotal("desserts");
+document.getElementById('DrinksTotal').innerHTML = "€" + GetCategoryTotal("drinks");
+
